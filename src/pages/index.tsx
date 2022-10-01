c06a3df7d1be6efbc26879a680b5ba1c0d95f855
Adam Petroff <adampetroff11235@gmail.com>
2022-10-01 12:29:30 +0200

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import TonConnector from "../components/Ton-Connector";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [userId, setUserId] = useState();
+
+  const routerQ = JSON.stringify(router.query);
+  useEffect(() => {
+    if (router.query.userId) {
+      setUserId(userId);
+    }
+  }, [routerQ]);
+
   return (
     <div className="App">
-      <h1>Ton Sample TWA</h1>
-      <TonConnector />
+      <h1>User id: {userId || "unrecognised"}</h1>
+      {/* <TonConnector /> */}
     </div>
   );
 };
